94e50d940aefc49e5149a3cc62429db35108529f
Nikolai Kabiolskij <n.kabiolskij@gmail.com>
2022-08-10 14:34:10 +0200

    removed app

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+//@ts-ignore
+import { Module, moduleStore } from "relocation-forms/dist";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Provider } from "react-redux";
+import { ModuleProps } from "relocation-forms/dist/types";
+
+const theme = createTheme({
+  palette: { primary: { main: "#2e69ff" } },
+});
+
+const App = () => {
+  const moduleProps: ModuleProps = {
+    backendUrl: process.env.REACT_APP_BACKEND!,
+    gapiEnabled: process.env.REACT_APP_GAPI_PLACES,
+    gapiKey: process.env.REACT_APP_GOOGLE_API_KEY!,
+    awsImageUploadEnabled: process.env.REACT_APP_PICTURE_UPLOAD,
+    awsPoolId: process.env.REACT_APP_AWS_POOL_ID!,
+    token: process.env.REACT_APP_API_TOKEN!,
+    tenant: "DEV",
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Provider store={moduleStore}>
+        <Module {...moduleProps} />
+      </Provider>
+    </ThemeProvider>
+  );
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
